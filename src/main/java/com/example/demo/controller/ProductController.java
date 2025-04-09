30d010e4af1fe1bfa7dc3d2b8e713257a807a3a3
sasa <sasa.pas@gamil.com>
2025-04-09 15:15:30 +0300

    Add very basic logging

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.repository.Product;
 import com.example.demo.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     private final ProductRepository repo;
 
     ProductController(ProductRepository repo) {
@@ -20,11 +23,15 @@ public class ProductController {
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        return repo.findAll();
+        List<Product> products = repo.findAll();
+        log.info("All existing products: " + products.toString());
+        return products;
     }
 
     @PostMapping("/product")
     public Product addProduct(@RequestBody Product newProduct) {
-        return repo.save(newProduct);
+        Product result = repo.save(newProduct);
+        log.info("Added product: " + result.toString());
+        return result;
     }
 }
